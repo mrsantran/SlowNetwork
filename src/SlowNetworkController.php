e9f0ac3fc7d2d6625a7944f559d19e6f1fd8c474
San Trần <santran686@gmail.com>
2016-12-19 15:11:19 +0700

    Update

--- src/SlowNetworkController.php
+++ src/SlowNetworkController.php
@@ -18,25 +18,25 @@ div.setAttribute("style", "text-align: center; width: 100%; position: absolute; 
 var slowLoad = window.setTimeout(function () {
     var div2 = document.createElement("div");
     var ct = document.createElement("center");
-    var t1 = document.createTextNode("' + config('slownetwork.text.taking.long.time') + '");
+    var t1 = document.createTextNode("' . config('slownetwork.text_taking_long_time') . '");
     var br = document.createElement("br");
-    var t2 = document.createTextNode("' + config('slownetwork.text.reload.page') + ' ");
+    var t2 = document.createTextNode("' . config('slownetwork.text_reload_page') . ' ");
     var a = document.createElement("a");
     a.setAttribute("href", document.URL);
-    a.innerHTML = "' + config('slownetwork.text.click.here') + '";
+    a.innerHTML = "' . config('slownetwork.text_click_here') . '";
     var dismiss = document.createElement("span");
-    dismiss.innerHTML = "' + config('slownetwork.text.dismiss') + '";
+    dismiss.innerHTML = "' . config('slownetwork.text_dismiss') . '";
     dismiss.setAttribute("class", "dismiss");
     dismiss.setAttribute("style", "float: right; margin-right: 3px; cursor: pointer;");
     dismiss.onclick = function () {
         html_node.removeChild(div);
-        document.body.style = "margin-bottom: ' + config('slownetwork.margin.bottom') + 'px;";
+        document.body.style = "margin-bottom: ' . config('slownetwork.margin_bottom') . 'px;";
     };
-    document.body.style = "margin-bottom: ' + config('slownetwork.margin.bottom') + 'px;margin-top: ' + config('slownetwork.margin.top') + 'px;";
+    document.body.style = "margin-bottom: ' . config('slownetwork.margin_bottom') . 'px;margin-top: ' . config('slownetwork.margin_top') . 'px;";
     var dismiss_container = document.createElement("div");
     dismiss_container.appendChild(dismiss);
     dismiss_container.setAttribute("class", "dismiss-container");
-    div2.setAttribute("style", "text-align: center; width: ' + config('slownetwork.width') + 'px; background: ' + config('slownetwork.color') + '; height: ' + config('slownetwork.hegiht') + 'px;");
+    div2.setAttribute("style", "text-align: center; width: ' . config('slownetwork.width') . 'px; background: ' . config('slownetwork.color') . '; height: ' . config('slownetwork.height') . 'px;");
     div.appendChild(ct);
     ct.appendChild(div2);
     div2.appendChild(t1);
@@ -45,11 +45,11 @@ var slowLoad = window.setTimeout(function () {
     div2.appendChild(a);
     div2.appendChild(dismiss_container);
     html_node.appendChild(div);
-}, ' + config('slownetwork.timeout') + ');
+}, ' . config('slownetwork.timeout') . ');
 
 window.addEventListener("load", function () {
     try {
-        document.body.style = "margin-bottom: ' + config('slownetwork.margin.bottom') + 'px;";
+        document.body.style = "margin-bottom: ' . config('slownetwork.margin_bottom') . 'px;";
         window.clearTimeout(slowLoad);
         html_node.removeChild(div);
     } catch (e) {
